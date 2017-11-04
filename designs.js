c6acf50e7bd95505a309afae8ca9261355f1b660
Armon Porter <armon.porter@gmail.com>
2017-11-03 23:09:39 -0400

    inputs created

--- designs.js
+++ designs.js
@@ -1,10 +1,25 @@
 // Select color input
+var color = document.getElementById('colorPicker').value;
 // Select size input
-
+var height = document.getElementById('input_height').value;
+var width = document.getElementById('input_width').value;
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
+document.getElementByIdfunction("sizePicker").addEventListner("submit"), function(){
+    makeGrid(height, width) {
+    var pixelCanvas = document.getElementById('pixel_canvas');
 // Your code goes here!
+var grid = ''; //starts string that will be canvas
+for (var h = 0; h < height; h++){
+  grid += '<tr>';
+    function makeGrid() {
+    for (var w = 0; w < width; w++){
+        grid += '<td></td>';
+      }
+      grid += '</tr>';
+    }
+    pixelCanvas.innerHTML = grid; //adds newly created grid to html pixel_canvas element
 
 }
+}
+}
+//Add Event Listners
